2b228316c76e1ef9db9a501196c9185cb195514a
Sanket Suryavanshi <82644048+sankets96@users.noreply.github.com>
2024-07-12 12:39:56 +0530

    Add files via upload

---
+++ 08_Asyncronus_javascript/01_async_functions.js
@@ -0,0 +1,14 @@
+//javascript is single thread and syncronus lang
+
+
+//settimeout
+
+setTimeout(()=>{
+console.log("Sanket suryavanshi");
+},2000);
+
+
+
+setInterval(()=>{
+console.log("this is interval of 1000");
+},1000);
